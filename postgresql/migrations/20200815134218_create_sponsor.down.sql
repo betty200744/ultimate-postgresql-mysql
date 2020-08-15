9cce5f9b6ba9a19c9f6f09ba8385143dc9684913
zhaoyl <htzhao200744@gmail.com>
2020-08-15 20:00:45 +0800

    [postgresql] update lexical structure

--- postgresql/migrations/20200815134218_create_sponsor.down.sql
+++ postgresql/migrations/20200815134218_create_sponsor.down.sql
@@ -1,2 +1,3 @@
-drop table donation;
+drop function increment;
 drop index sponsor_serial_number_index;
+drop table sponsor;
--- postgresql/migrations/20200815134218_create_sponsor.up.sql
+++ postgresql/migrations/20200815134218_create_sponsor.up.sql
@@ -1,9 +1,16 @@
+create or replace function increment(i integer) returns integer as
+$$
+begin
+    return i + 1; -- operators
+end;
+$$ language plpgsql; -- Constants Dollar-Quoted String Constants
 create table if not exists sponsor
 (
     id            uuid                     not null primary key default gen_random_uuid(),
-    serial_number varchar(255)             not null             default ''::varchar,
+    serial_number varchar(255)             not null             default ''::varchar, -- type case
     project_id    uuid                     not null,
-    fee           int8                     not null             default 0,
+    amount        real                     not null             default 0,           --float
+    fee           int8                     not null             default 0,           --bigint
     sponsor_time  timestamp with time zone not null             default current_timestamp,
     create_time   timestamp with time zone not null             default current_timestamp,
     misc          jsonb                    not null             default '{}'::jsonb
