695b9d63e24e37f1d37131251d559229d29d9f63
zhaoyl <htzhao200744@gmail.com>
2020-08-17 16:31:25 +0800

    [postgresql][dml] add data type array

--- postgresql/migrations/20200817080450_alter_projects_add_version_control.down.sql
+++ postgresql/migrations/20200817080450_alter_projects_add_version_control.down.sql
@@ -1,2 +1,4 @@
 alter table projects
     drop column version_control;
+alter table projects
+    drop column tags;
--- postgresql/migrations/20200817080450_alter_projects_add_version_control.up.sql
+++ postgresql/migrations/20200817080450_alter_projects_add_version_control.up.sql
@@ -1,2 +1,4 @@
 alter table projects
     add column version_control varchar(50) not null default '';
+alter table projects
+    add column tags varchar[];
