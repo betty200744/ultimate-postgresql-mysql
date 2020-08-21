f507f49b9486c4cfc0d4a50cf399902b9dad9a7c
zhaoyl <htzhao200744@gmail.com>
2020-08-21 10:02:43 +0800

    [mysql][queries] add queries_employees.sql and queries_system.sql

---
+++ mysql/queries/queries_employees.sql
@@ -0,0 +1,11 @@
+--  /*! STRAIGHT_JOIN */
+explain select  * from employees , departments where employees.employees.emp_no = 10001;
+-- KEY_BLOCK_SIZE
+CREATE TABLE if not exists t3(a INT, KEY (a));
+analyze table employees;
+check table employees;
+optimize table employees;
+
+explain select  * from employees , departments where employees.employees.emp_no = 10001;
+
+show table status ;
---
+++ mysql/queries/queries_system.sql
@@ -0,0 +1,8 @@
+select version(), current_timestamp;
+select (4 + 1) * 5;
+select now();
+select user(), current_timestamp;
+show databases ;
+
+select database();
+show tables ;
